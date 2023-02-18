2e6853f1b5d3bfdb6fa9c48f18872b43b21f72b7
UjasJoshi1 <103307597+UjasJoshi1@users.noreply.github.com>
2023-02-18 14:55:17 +0530

    Update first.py

--- CodeKrackers/first.py
+++ CodeKrackers/first.py
@@ -1,19 +1,16 @@
 import pygame
 from pygame import mixer
-
 pygame.init()
-
 WIDTH = 1400
 HEIGHT = 800
-
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
-
+green=(0,255,0)
+gold=(212,175,55)
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('music kit')
 label_font = pygame.font.Font('FreeSansBold.ttf', 30)
-
 fps = 60
 timer = pygame.time.Clock()
 beats = 8
@@ -28,32 +25,35 @@ def draw_grid():
     colors = [gray, white, white]
     hi_hat_text = label_font.render('Hi Hat', True, white)
     screen.blit(hi_hat_text, (50, 30))
-
     Snare_text = label_font.render('Snare', True, white)
     screen.blit(Snare_text, (50, 130))
-
     kick_text = label_font.render('Bass Drum', True, white)
     screen.blit(kick_text, (24, 230))
-
     crash_text = label_font.render('Crash', True, white)
     screen.blit(crash_text, (53, 330))
-
     clap_text = label_font.render('Clap', True, white)
     screen.blit(clap_text, (65, 430))
-
     floor_tom_text = label_font.render('Floor Tom', True, white)
     screen.blit(floor_tom_text, (30, 530))
-
     for i in range(instruments):
         pygame.draw.line(screen, gray, (0, (i * 100) + 100), (200, (i * 100) + 100), 3)
 
 
+# for i in range(beats):
+#     for j in range(instruments):
+#         rect = pygame.draw.rect(screen, gray, [i * ((WIDTH - 200) // beats) + 205, (j * 100), ((WIDTH - 200) // beats), ((HEIGHT - 200)//instruments)], 5, 5)
+
     for i in range(beats):
         for j in range(instruments):
-            rect = pygame.draw.rect(screen, gray, [i * ((WIDTH - 200) // beats) + 205, (j * 100), ((WIDTH - 200) // beats), ((HEIGHT - 200)//instruments)], 5, 5)
+            if clicked[j][i]==-1:
+                color=gray
+            else:
+                color=green
+            rect = pygame.draw.rect(screen, color, [i * ((WIDTH - 200) // beats) + 210, (j * 100)+5, ((WIDTH - 200) // beats)-10, ((HEIGHT - 200)//instruments)-10], 0,3 )
+            pygame.draw.rect(screen, gold, [i * ((WIDTH - 200) // beats) + 205, (j * 100), ((WIDTH - 225) // beats), ((HEIGHT - 200)//instruments)], 5, 5)
+            pygame.draw.rect(screen, black, [i * ((WIDTH - 200) // beats) + 205, (j * 100), ((WIDTH - 200) // beats), ((HEIGHT - 200)//instruments)], 2, 5)
             boxes.append((rect, (i, j)))
     return boxes
-
 
 
 
@@ -61,18 +61,19 @@ run = True
 while run:
     timer.tick(fps)
     screen.fill(black)
+    draw_grid()
     boxes = draw_grid()
 
     for event in pygame.event.get(): #any event taking place
         if event.type == pygame.QUIT:
             run = False
-        
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             for i in range(len(boxes)):
                 if boxes[i][0].colliderect(event.pos): #position at which mouse down occur
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1
-    
+
+
     pygame.display.flip()
 pygame.quit()
-
