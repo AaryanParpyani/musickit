cc766f1ce9c3fc07164b38773052f03b3a7a53a4
UjasJoshi1 <103307597+UjasJoshi1@users.noreply.github.com>
2023-02-19 08:52:39 +0530

    Update first.py

--- CodeKrackers/first.py
+++ CodeKrackers/first.py
@@ -10,6 +10,7 @@ green=(0,255,0)
 gold=(212,175,55)
 blue=(0,255,255)
 dark_gray = (50, 50, 50)
+light_gray=(170,170,170)
 
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
@@ -33,6 +34,7 @@ beat_changed=True
 save_menu=False
 load_menu=False
 saved_beats=[]
+index=100
 file=open('saved_beats.txt','r')
 for line in file:
     saved_beats.append(line)
@@ -141,7 +143,10 @@ def draw_save_menu(beat_name,typing):
     return exit_btn,saving_btn,entry_rect
 
 
-def draw_load_menu():
+def draw_load_menu(index):
+     loaded_clicked=[]
+     loaded_beats=0
+     loaded_bpm=0
      pygame.draw.rect(screen,black,[0,0,WIDTH,HEIGHT])
      menu_text=label_font.render('LOAD MENU: Select a Beat TO Load',True,white)
      loading_btn=pygame.draw.rect(screen,gray,[WIDTH // 2-200,HEIGHT*0.87,400,100],0,5)
@@ -154,8 +159,34 @@ def draw_load_menu():
      exit_btn=pygame.draw.rect(screen,gray,[WIDTH-200,HEIGHT-100,180,90],0,5)
      exit_text=label_font.render('Close',True,white)
      screen.blit(exit_text, (WIDTH-160,HEIGHT-70))
-     pygame.draw.rect(screen,gray,[190,90,1000,600],5,5)
-     return exit_btn,loading_btn,delete_btn
+     loaded_rectangle=pygame.draw.rect(screen,gray,[190,90,1000,600],5,5)
+     if 0<= index < len(saved_beats):
+         pygame.draw.rect(screen,light_gray,[190,100+index*50,1000,50])
+     for beat in range(len(saved_beats)):
+         if beat<10:
+             beat_clicked=[]
+             row_text=medium_font.render(f'{beat+1}',True,white)
+             screen.blit(row_text,(200,100+beat*50))
+             name_index_start=saved_beats[beat].index(', name: ')+6
+             name_index_end=saved_beats[beat].index(', beats:')
+             name_text=medium_font.render(saved_beats[beat][name_index_start:name_index_end],True,white)
+             screen.blit(name_text,(240,100+beat*50))
+         if 0<=index<len(saved_beats):
+             beat_index_end=saved_beats[beat].index(', bpm:')
+             loaded_beats=int(saved_beats[beat][name_index_end+8: beat_index_end])
+             bpm_index_end=saved_beats[beat].index(', selected:')
+             loaded_bpm=int(saved_beats[beat][beat_index_end+6: bpm_index_end])
+             loaded_clicks_string=saved_beats[beat][bpm_index_end+14: -3]
+             loaded_clicks_rows=list(loaded_clicks_string.split('],['))
+             for row in range(len(loaded_clicks_rows)):
+                  loaded_clicks_row=(loaded_clicks_rows[row].split(', '))
+                  for item in range(len(loaded_clicks_row)):
+                      if loaded_clicks_row[item]=='1' or loaded_clicks_row[item]=='-1':
+                          loaded_clicks_row[item]=int(loaded_clicks_row[item])
+                  beat_clicked.append(loaded_clicks_row)
+                  loaded_clicked=beat_clicked
+     loaded_info=[loaded_beats,loaded_bpm,loaded_clicked]
+     return exit_btn,loading_btn,delete_btn,loaded_rectangle,loaded_info
 
 
 
@@ -230,7 +261,7 @@ while run:
     if save_menu:
         exit_button,saving_button,entry_rectangle=draw_save_menu(beat_name,typing)
     if load_menu:
-        exit_button,loading_button,delete_button=draw_load_menu()
+        exit_button,loading_button,delete_button,loaded_rectangle,loaded_info=draw_load_menu(index)
 
     if beat_changed:
         play_notes()
@@ -281,12 +312,26 @@ while run:
                  playing=True
                  beat_name=''
                  typing=False
-             elif entry_rectangle.collidepoint(event.pos):
+             if load_menu:
+              if loaded_rectangle.collidepoint(event.pos):
+                 index=(event.pos[1]-100)//50
+             if delete_button.collidepoint(event.pos):
+                 if 0<=index<len(saved_beats):
+                     saved_beats.pop(index)
+             if loading_button.collidepoint(event.pos):
+                 if 0<=index < len(saved_beats):
+                     beats=loaded_info[0]
+                     bpm=loaded_info[1]
+                     clicked=loaded_info[2]
+                     index=100
+                     load_menu=False
+             if save_menu:
+              if entry_rectangle.collidepoint(event.pos):
                  if typing:
                      typing=False
                  elif not typing:
                      typing=True
-             elif saving_button.collidepoint(event.pos):
+             if saving_button.collidepoint(event.pos):
                  file=open('saved_beats.txt','w')
                  saved_beats.append(f'\nname: {beat_name}, beats: {beats}, bpm: {bpm}, selected: {clicked}')
                  for i in range(len(saved_beats)):
